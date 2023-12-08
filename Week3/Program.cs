a3079376037144dc75162f4e06ccc43533409a8e
leonmhl <leonmoehl@outlook.de>
2023-12-08 16:52:21 +0200

    Week3

---
+++ Week3/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+using System.ComponentModel.Design;
+
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+Console.WriteLine(weekDay);
+
+// switch method
+switch (weekDay)
+{
+    case 0:
+        Console.WriteLine("Sunday");
+        break;
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;  
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;  
+    case 4:
+        Console.WriteLine("Thursday");
+        break;  
+    case 5:
+        Console.WriteLine("Friday");
+        break;  
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Invalid value")
+}
+
+// if method
+
+//if (weekDay == 0)
+//{
+//    Console.WriteLine("Sunday");
+//}
+
+//else if (weekDay == 1)
+//{
+//    Console.WriteLine("Monday");
+//}
+
+//else if (weekDay == 2)
+//{
+//    Console.WriteLine("Tuesday");
+//}
+
+//else if (weekDay == 3)
+//{
+//    Console.WriteLine("Wednesday");
+//}
+
+//else if (weekDay == 4)
+//{
+//    Console.WriteLine("Thursday");
+//}
+
+//else if (weekDay == 5)
+//{
+//    Console.WriteLine("Friday");
+//}
+
+//else
+//{
+//    Console.WriteLine("Saturday");
+//}
---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿//Odd or even -> ungerade oder gerade
+
+int a = 11;
+ 
+
+int result = a % 2;             //% shows the remainder after divison with 2
+
+Console.WriteLine(result);
+
+//switch method
+switch(result)
+{
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+    case 1:
+        Console.WriteLine($"{a} is an odd number");
+        break;
+    default:
+        Console.WriteLine("Invalid value");
+        break;
+}
+
+
+//if method
+
+//if(result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+//}
+//else
+//{
+//    Console.WriteLine($"{a} is an odd number");
+//}
